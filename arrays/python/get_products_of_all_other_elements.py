f15a9eb2bc5a2251f6937419ad48f96095e9a487
Vitor Oliveira <vbrazo@gmail.com>
2020-12-09 18:36:34 -0800

    Extract code to files

---
+++ arrays/python/get_products_of_all_other_elements.py
@@ -0,0 +1,48 @@
+# Python implementation
+
+# Generates prefix products
+def build_prefix_products(nums):
+  prefix_products = []
+
+  for num in nums:
+    if prefix_products:
+      prefix_products.append(prefix_products[-1] * num)
+    else:
+      prefix_products.append(num)
+  return prefix_products
+
+# Generates suffix products
+def build_suffix_products(nums):
+  suffix_products = []
+
+  for num in reversed(nums):
+    if suffix_products:
+      suffix_products.append(suffix_products[-1] * num)
+    else:
+      suffix_products.append(num)
+
+  return suffix_products
+
+# Builds output
+def output(prefix_products, suffix_products, nums):
+  result = []
+
+  for i in range(len(nums)):
+    if i == 0:
+      result.append(suffix_products[i + 1])
+    elif i == len(nums) - 1:
+      result.append(prefix_products[i - 1])
+    else:
+      result.append(prefix_products[i - 1] * suffix_products[i + 1])
+  return result
+
+# Generates result from the product of prefixes and suffixes
+def products(nums):
+  prefix_products = build_prefix_products(nums)
+  suffix_products = build_suffix_products(nums)
+  suffix_products = list(reversed(suffix_products))
+
+  return output(prefix_products, suffix_products, nums)
+
+print(products([1, 2, 3]))
+# => [6, 3, 2]
---
+++ arrays/ruby/get_products_of_all_other_elements.rb
@@ -0,0 +1,62 @@
+# Arrays - Get Products of all other elements
+
+# Ruby implementation
+
+# Generates prefix products
+def build_prefix_products(nums)
+  prefix_products = []
+
+  nums.each do |num|
+    if prefix_products.count > 0
+      prefix_products.append(prefix_products.last * num)
+    else
+      prefix_products.append(num)
+    end
+  end
+
+  return prefix_products
+end
+
+# Generates suffix products
+def build_suffix_products(nums)
+  suffix_products = []
+
+  nums.reverse.each do |num|
+    if suffix_products.count > 0
+      suffix_products.append(suffix_products.last * num)
+    else
+      suffix_products.append(num)
+    end
+  end
+
+  return suffix_products
+end
+
+# Builds output
+def output(prefix_products, suffix_products, nums)
+  result = []
+
+  nums.reverse.each_with_index do |num, index|
+    if index == 0
+      result.append(suffix_products[index + 1])
+    elsif index == nums.length - 1
+      result.append(prefix_products[index - 1])
+    else
+      result.append(prefix_products[index - 1] * suffix_products[index + 1])
+    end
+  end
+
+  return result
+end
+
+# Generate result from the product of prefixes and suffixes
+def products(nums)
+  prefix_products = build_prefix_products(nums)
+  suffix_products = build_suffix_products(nums)
+  suffix_products = suffix_products.reverse
+
+  return output(prefix_products, suffix_products, nums)
+end
+
+puts(products([1, 2, 3]))
+# => [6, 3, 2]
