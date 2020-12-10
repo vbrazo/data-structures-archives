dae010d1201b9ae062c0cd9faf400b0e8e8cdfdf
Vitor Oliveira <vbrazo@gmail.com>
2020-12-09 22:57:45 -0800

    Add better scenario

--- strings/python/find_anagram_indices.py
+++ strings/python/find_anagram_indices.py
@@ -16,3 +16,41 @@ def anagram_indices(word, s):
     return result
 
 print(anagram_indices("ab", "abxaba"))
+
+# Better scenario
+# This second algorithm runs in O(s) time and space.
+from collections import defaultdict
+
+def del_if_zero(dict, char):
+    if dict[char] == 0:
+        del dict[char]
+
+def anagram_indices(word, s):
+    result = []
+    freq = defaultdict(int)
+
+    for char in word:
+        freq[char] += 1
+
+    for char in s[:len(word)]:
+        freq[char] -= 1
+        del_if_zero(freq, char)
+
+    if not freq:
+        result.append(0)
+
+    for i in range(len(word), len(s)):
+        start_char, end_char = s[i - len(word)], s[i]
+        freq[start_char] += 1
+        del_if_zero(freq, start_char)
+
+        freq[end_char] -= 1
+        del_if_zero(freq, end_char)
+
+        if not freq:
+            beginning_index = i - len(word) + 1
+            result.append(beginning_index)
+
+    return result
+
+print(anagram_indices("ab", "abxaba"))
