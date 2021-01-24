bf6b0998d332670fd546affb779f12ce62e731fa
Vitor Oliveira <vbrazo@gmail.com>
2021-01-24 11:19:32 -0800

    Change linked list logic

--- data-structures/linked_list/design_linked_list.rb
+++ data-structures/linked_list/design_linked_list.rb
@@ -13,117 +13,96 @@
 #
 
 class MyLinkedList
-  attr_accessor :head, :size
+  attr_accessor :head
 
   def initialize()
     @head = nil
-    @size = 0
   end
 
   # Get the value of the index-th node in the linked list. If the index is invalid, return -1.
   def get(index)
-    return -1 if index > @size - 1 || index < 0
+    i = 0
+    head = @head
 
-    current = index_position(index)
-    current.value
+    while !head.nil? && i<index
+      return -1 if head.nil?
+      head = head.next
+      i+=1
+    end
+
+    case head
+      when nil then return -1
+      else return head.val
+      end
   end
 
   # Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
   def add_at_head(val)
-    new_node = Node.new(val)
-    new_node.next = @head if @head
-    @head = new_node
-    @size += 1
+    new_head = ListNode.new(val)
+    new_head.next = @head
+    @head = new_head
   end
-
 
   # Append a node of value val to the last element of the linked list.
   def add_at_tail(val)
-    current = @head
-    add_at_head(val) unless current
-    new_node = Node.new(val)
-
-    while current.next
-      current = current.next
-    end
-
-    current.next = new_node
-    @size += 1
+    new_tail = ListNode.new(val)
+    head = @head
+    case @head
+      when nil then @head = new_tail
+      else
+        while !head.next.nil?
+          head = head.next
+        end
+        head.next = new_tail
+      end
   end
 
-  # Add a node of value val before the index-th node in the linked list.
-  # If index equals to the length of linked list, the node will be appended
-  # to the end of linked list. If index is greater than the length, the node
-  # will not be inserted.
+  # Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
   def add_at_index(index, val)
-    return if index > @size
+    node = ListNode.new(val)
 
-    if index.zero? || index < 0
-      add_at_head(val)
-      return
-    elsif index == @size
-      add_at_tail(val)
-      return
-    end
-
-    current = previous_index_position(index)
-    tmp = current.next
-    new_node = Node.new(val)
-    new_node.next = tmp
-    current.next = new_node
-    @size += 1
+    case index
+      when 0
+      then
+        node.next = @head
+        @head = node
+      else
+        i = 0
+        head = @head
+        while i<index-1
+            return if head.nil?
+            head = head.next
+            i+=1
+        end
+        return if head.nil?
+        node.next = head.next
+        head.next = node
+      end
   end
 
   # Delete the index-th node in the linked list, if the index is valid.
   def delete_at_index(index)
-    return if index > @size - 1 || index < 0
-
-    if index.zero? && @size == 1
-      @head = nil
-      @size = 0
-      return
+    return if @head.nil?
+    case index
+    when 0 then @head = @head.next
+    else
+      i = 0
+      head = @head
+      while i < index-1
+        return if head.nil?
+        head = head.next
+        i+=1
+      end
+      return if head.next.nil?
+      head.next = head.next.next
     end
-
-    delete_at_head && return if index.zero?
-
-    current = previous_index_position(index)
-    current.next = nil && return unless current.next
-    current.next = current.next.next
-    @size -= 1
-  end
-
-  def index_position(index)
-    i = 0
-    current = @head
-    while i < index
-      current = current.next
-      i += 1
-    end
-    current
-  end
-
-  def previous_index_position(index)
-    i = 0
-    current = @head
-    while i < index - 1
-      current = current.next
-      i += 1
-    end
-    current
-  end
-
-  def delete_at_head
-    next_node = @head.next
-    @head = next_node
-    @size -= 1
   end
 end
 
-class Node
-  attr_accessor :value, :next
-
+class ListNode
+  attr_accessor :val, :next
   def initialize(val)
-    @value = val
+    @val = val
     @next = nil
   end
 end
