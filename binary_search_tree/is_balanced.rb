a695a21c65776cd62577cc9bedbf23422824cdb7
Vitor Oliveira <vbrazo@gmail.com>
2021-05-30 04:40:13 -0700

    clean up

--- binary_search_tree/is_balanced.rb
+++ binary_search_tree/is_balanced.rb
@@ -29,16 +29,17 @@ end
 # @param {TreeNode} root
 # @return {Boolean}
 def is_balanced(root)
-   return true if (root.nil?)     # If the tree is empty it is valid
-   left = get_height(root.left)   # Get the height of the left side of this specific node
-   right = get_height(root.right) # Same as above for the right
-   min = [left, right].min        # Get the smallest height of this specific node
-   max = [left, right].max        # Get the biggest height
+  return true if root.nil? # If the tree is empty it is valid
 
-   # Here we make sure the height difference is at the most One,
-   # and we recursively check Every Single Node in our tree using the same
-   # methods above by recursively calling it on our children below.
-   return (max - min <= 1) && is_balanced(root.left) && is_balanced(root.right)
+  left = get_height(root.left)   # Get the height of the left side of this specific node
+  right = get_height(root.right) # Same as above for the right
+  min = [left, right].min        # Get the smallest height of this specific node
+  max = [left, right].max        # Get the biggest height
+
+  # Here we make sure the height difference is at the most One,
+  # and we recursively check Every Single Node in our tree using the same
+  # methods above by recursively calling it on our children below.
+  (max - min <= 1) && is_balanced(root.left) && is_balanced(root.right)
 end
 
 def get_height(root)
@@ -48,5 +49,5 @@ def get_height(root)
   # Recursively compare the height of the left side and the right side of our
   # tree and return the max height. Remember the recursive function ends up
   # returning an integer allowing us to Add it to + 1.
-  return [get_height(root.left) + 1, get_height(root.right) + 1].max
+  [get_height(root.left) + 1, get_height(root.right) + 1].max
 end
