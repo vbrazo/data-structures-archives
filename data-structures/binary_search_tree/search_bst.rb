8aa940317e04e0b436ac6d0c9eb01a43951123a7
Vitor Oliveira <vbrazo@gmail.com>
2021-05-30 02:22:00 -0700

    Add iterative approach

--- data-structures/binary_search_tree/search_bst.rb
+++ data-structures/binary_search_tree/search_bst.rb
@@ -45,3 +45,39 @@ def search_bst(root, val)
     search_bst(root.right, val)
   end
 end
+
+#
+# Approach 2: Iteration (Time Limit Exceeded)
+#
+
+# Algorithm
+# To reduce the space complexity, one could convert recursive approach into the iterative one:
+#
+# While the tree is not empty root != null and the value to find is not here val != root.val:
+#
+# If val < root.val - go to search into the left subtree root = root.left.
+# If val > root.val - go to search into the right subtree root = root.right.
+#
+# Return root.
+
+# Complexity Analysis
+#
+# Time complexity: O(H), where H is a tree height. That results in O(log n) in
+# the average case, and O(N) in the worst case.
+#
+# Space complexity: O(1) since it's a constant space solution.
+
+# @param {TreeNode} root
+# @param {Integer} val
+# @return {TreeNode}
+def search_bst(root, val)
+  while !root.nil? and root.val != val
+    if val < root.val
+      root = root.left
+    else
+      root.right
+    end
+  end
+
+  root
+end
